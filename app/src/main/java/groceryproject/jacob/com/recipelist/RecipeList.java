81c335bbe2ca46ceb76c3b7883ca44c18fbf984d
Jacob <jacobschioppo@yahoo.com>
2017-01-10 18:16:56 -0500

    Created Actionbar Button Need to fix namespace issue (how to create custom XML namespace)

--- app/src/main/java/groceryproject/jacob/com/recipelist/RecipeList.java
+++ app/src/main/java/groceryproject/jacob/com/recipelist/RecipeList.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Parcelable;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
